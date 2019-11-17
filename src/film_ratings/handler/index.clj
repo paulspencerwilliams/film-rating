184b88914ef3819c9da3358d200dc3a5acdbefd8
Paul Williams <paul@paulswilliams.me.uk>
2019-11-17 15:27:16 +0000

    Implement create and view created film including boundary and migration

--- src/film_ratings/handler/index.clj
+++ src/film_ratings/handler/index.clj
@@ -1,6 +1,5 @@
 (ns film-ratings.handler.index
-  (:require [ataraxy.core :as ataraxy]
-            [ataraxy.response :as response]
+  (:require [ataraxy.response :as response]
             [film-ratings.views.index :as views.index]
             [integrant.core :as ig]))
 
