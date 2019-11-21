954ee922661ce5ba2a2af0cb8b99cbe049b64e52
Paul Williams <paul@paulswilliams.me.uk>
2019-11-21 06:33:37 +0000

    Run migrations on application startup

--- src/film_ratings/main.clj
+++ src/film_ratings/main.clj
@@ -5,8 +5,9 @@
 (duct/load-hierarchy)
 
 (defn -main [& args]
-  (let [keys     (or (duct/parse-keys args) [:duct/daemon])
+  (let [keys (or (duct/parse-keys args) [:duct/migrator :duct/daemon])
         profiles [:duct.profile/prod]]
+    (prn keys)
     (-> (duct/resource "film_ratings/config.edn")
         (duct/read-config)
         (duct/exec-config profiles keys))))
